5ba82d8bed88ba44084c7b6f29447165f16aee1a
j.cordes <j.cordes@fz-juelich.de>
2022-09-07 17:32:22 +0200

    Notebook added

--- Library.jl
+++ Library.jl
@@ -1,6 +1,7 @@
 include("./Structure.jl")
 include("./Vectors.jl")
 include("./Field_Eta.jl")
+include("./Energy.jl")
 include("./Init_Agent.jl")
 include("./Init_Obstacle.jl")
 include("./Optimization_Algorithm.jl");
